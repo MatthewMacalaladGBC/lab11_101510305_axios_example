9c2833a89e71baeef132d4d0aa33dcedc2817843
MatthewMacalaladGBC <matthew.macalalad@georgebrown.ca>
2025-11-17 15:30:21 -0500

    completed week 11 lab exercise

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/PersonList'
+import PersonList from './components/PersonList';
 
 function App() {
   return (
     <div className="App">
-      
+      <PersonList></PersonList>
     </div>
   );
 }
--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -1,3 +1,4 @@
+import "./PersonList.css"
 import React, { Component } from "react"
 import axios from "axios";
 
@@ -16,12 +17,63 @@ export default class PersonList extends Component {
             const persons = res.data.results;
             this.setState({ persons });
         })
-        .catch(err => console.log(err));
+        .catch(err => console.error('There was an error fetching the users!', err));
     }
 
     render() {
         return(
-            <div>
+            <div style={{ padding: "15px" }}>
+                <h1 className="page-title">User List</h1>
+
+                {this.state.persons.map((p, index) => (
+                    
+                    <div key={index} className="user-card">
+                        <div className="user-card-header">
+                            {p.name.title} {p.name.first} {p.name.last} – {p.login.uuid || "N/A"}
+                        </div>
+
+                        <div className="user-card-body">
+                            <div>
+                                <img
+                                    src={p.picture.large}
+                                    alt="user's profile image"
+                                    className="user-image"
+                                />
+                            </div>
+                            <div className="user-info">
+                                <span class="user-info-label">User Name:</span>
+                                <span class="user-info-value">{p.login.username}</span>
+
+                                <span class="user-info-label">Gender:</span>
+                                <span class="user-info-value">{p.gender.toUpperCase()}</span>
+
+                                <span class="user-info-label">Time Zone Description:</span>
+                                <span class="user-info-value">{p.location.timezone.description}</span>
+
+                                <span class="user-info-label">Address:</span>
+                                <span class="user-info-value">
+                                    {p.location.street.number} {p.location.street.name}, {p.location.city}, {p.location.state}, {p.location.country} - {p.location.postcode}
+                                </span>
+
+                                <span class="user-info-label">Email:</span>
+                                <span class="user-info-value">{p.email}</span>
+
+                                <span class="user-info-label">Birth Date and Age:</span>
+                                <span class="user-info-value">{p.dob.date} ({p.dob.age})</span>
+
+                                <span class="user-info-label">Register Date:</span>
+                                <span class="user-info-value">{p.registered.date}</span>
+
+                                <span class="user-info-label">Phone #:</span>
+                                <span class="user-info-value">{p.phone}</span>
+
+                                <span class="user-info-label">Cell #:</span>
+                                <span class="user-info-value">{p.cell}</span>
+                            </div>
+                        </div>
+                    </div>
+
+                ))}
 
             </div>
         );
